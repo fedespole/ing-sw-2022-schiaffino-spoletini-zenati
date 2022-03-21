99b56d5ba5a8144cff82768245092dba3a160c15
fedespole <fedespo@live.it>
2022-03-21 20:04:31 +0100

    added characters

---
+++ src/main/java/it/polimi/ingsw/model/expertgame/characters/Character1.java
@@ -0,0 +1,38 @@
+package it.polimi.ingsw.model.expertgame.characters;
+
+import it.polimi.ingsw.model.basicgame.Island;
+import it.polimi.ingsw.model.basicgame.Student;
+import it.polimi.ingsw.model.expertgame.ConcreteExpertGame;
+
+import java.util.ArrayList;
+
+public class Character1 extends Character {
+
+    private ArrayList<Student> students;
+
+    public Character1(ConcreteExpertGame currGame){
+
+        students = new ArrayList<Student>();
+        for(int i=0;i<4;i++) {
+            addStudent(currGame.getGame().getBag().removeStudent());
+        }
+    }
+
+    public void useAbility(ConcreteExpertGame currGame, Student student, Island island){
+        removeStudent(student);
+        island.addStudent(student);
+        addStudent(currGame.getGame().getBag().removeStudent());
+    }
+
+    public ArrayList<Student> getStudents() {
+        return students;
+    }
+
+    public void removeStudent(Student student){
+        students.remove(student);
+    }
+
+    public void addStudent(Student student){
+        students.add(student);
+    }
+}
