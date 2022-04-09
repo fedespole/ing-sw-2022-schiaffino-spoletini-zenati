a1d6724e73246cef6920df7abbe7917b450d53ee
AlbertoSchiaffino <alberto1.schiaffino1@mail.polimi.it>
2022-04-09 17:45:29 +0200

    First tests

--- src/test/java/it/polimi/ingsw/controller/ControllerTest.java
+++ src/test/java/it/polimi/ingsw/controller/ControllerTest.java
@@ -1,9 +1,12 @@
 package it.polimi.ingsw.controller;
 
+import it.polimi.ingsw.common.events.DrawAssistantCardEvent;
 import it.polimi.ingsw.common.events.PlayerAccessEvent;
+import it.polimi.ingsw.common.events.StartGameEvent;
 import it.polimi.ingsw.model.basicgame.BasicGame;
 import it.polimi.ingsw.model.basicgame.Game;
 import it.polimi.ingsw.model.basicgame.playeritems.Player;
+import it.polimi.ingsw.model.expertgame.ConcreteExpertGame;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -13,12 +16,11 @@ import static org.junit.Assert.*;
 
 public class ControllerTest {
 
-    private Game game;
     private Controller controller;
 
     @BeforeEach
     public void setUp(){
-        game =new BasicGame(new Player("Host"));
+        Game game =new BasicGame(new Player("Host"));
         this.controller = new Controller(game);
     }
 
@@ -26,10 +28,38 @@ public class ControllerTest {
     public void PlayerAccessEventTest() {
         PlayerAccessEvent event = new PlayerAccessEvent(this,"Test1");
         controller.update(event);
-        assertEquals("Test1",game.getPlayers().get(1).getUsername());
+        assertEquals("Test1",controller.getGame().getPlayers().get(1).getUsername());
         event = new PlayerAccessEvent(this,"Test2");
         controller.update(event);
-        assertEquals("Test2",game.getPlayers().get(2).getUsername());
+        assertEquals("Test2",controller.getGame().getPlayers().get(2).getUsername());
 
+    }
+
+    @Test
+    public void StartBasicGameEventTest(){
+        this.PlayerAccessEventTest();
+        StartGameEvent event= new StartGameEvent(this,false);
+        controller.update(event);
+        assertTrue(controller.getGame() instanceof BasicGame);
+    }
+
+    @Test
+    public void StartExpertGameEventTest(){
+        this.PlayerAccessEventTest();
+        StartGameEvent event= new StartGameEvent(this,true);
+        controller.update(event);
+        assertTrue(controller.getGame() instanceof ConcreteExpertGame);
+    }
+    @Test
+    public void DrawAssistantCardEventTest(){
+        this.StartBasicGameEventTest();
+        DrawAssistantCardEvent event = new DrawAssistantCardEvent(this,3);
+        Player firstPlayer = controller.getGame().getCurrPlayer();
+        controller.update(event);
+        assertEquals(3,firstPlayer.getChosenCard().getValue());
+        Player second_player = controller.getGame().getCurrPlayer();
+        event = new DrawAssistantCardEvent(this,5);
+        controller.update(event);
+        assertEquals(5,second_player.getChosenCard().getValue());
     }
 }
