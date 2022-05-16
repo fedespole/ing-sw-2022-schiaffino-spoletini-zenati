8e145dd2286fdab5dc3c906d9d883cd8e2f4ee34
AlbertoSchiaffino <alberto1.schiaffino1@mail.polimi.it>
2022-05-16 22:08:17 +0200

    Planning scene

--- src/main/java/it/polimi/ingsw/view/gui/controllers/PlanningController.java
+++ src/main/java/it/polimi/ingsw/view/gui/controllers/PlanningController.java
@@ -1,5 +1,8 @@
 package it.polimi.ingsw.view.gui.controllers;
 
+import it.polimi.ingsw.model.basicgame.*;
+import it.polimi.ingsw.model.basicgame.playeritems.AssistantCard;
+import it.polimi.ingsw.model.basicgame.playeritems.Player;
 import it.polimi.ingsw.view.gui.GuiManager;
 import javafx.fxml.FXML;
 import javafx.geometry.Pos;
@@ -7,23 +10,42 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.GridPane;
 
 import java.io.File;
+import java.util.ArrayList;
 
 public class PlanningController extends GuiController{
 
     public FlowPane AssistantCards;
-    public ImageView AssistantCard0;
+    public GridPane MyDiningRoom;
+    public GridPane MyEntrance;
+    public GridPane MyProfessors;
+    public GridPane MyTowers;
+    public GridPane Player1DiningRoom;
+    public GridPane Player1Professors;
+    public GridPane Player1Entrance;
+    public GridPane Player1Towers;
+    public ImageView Player1AssistantCard;
+    public GridPane Player2Entrance;
+    public GridPane Player2Towers;
+    public GridPane Player2DiningRoom;
+    public ImageView Player2Board;
+    public GridPane Player2Professors;
+    public ImageView Player2AssistantCard;
 
     @FXML
+    @Override
     public void initialize() {
         super.initialize();
         guiManager.getStage().setFullScreen(true);
-        Image image = new Image(new File("@../graphics/playerItems/deck/assistantCards/Assistente (1).png").toURI().toString());
-        ImageView imageView = new ImageView(image);
-        imageView.setFitWidth(150);
-        imageView.setFitHeight(0);
-        AssistantCards.getChildren().add(imageView);
+        if(guiManager.getData().getNumPlayers()==3){
+            Image image= new Image(GuiManager.class.getResource("/graphics/playerItems/schoolBoard/Plancia_DEF3.png").toString());
+            Player2Board.setImage(image);
+        }
+        this.fillMyDiningRoom();
+        this.addAvailableAssistantCards();
+        this.fillOtherPlayers();
     }
 
 
@@ -34,5 +56,139 @@ public class PlanningController extends GuiController{
 
     public void mouseOffCharacters(MouseEvent mouseEvent) {
         AssistantCards.alignmentProperty().setValue(Pos.BOTTOM_CENTER);
+    }
+
+    private void addAvailableAssistantCards() {
+        for (Player player : guiManager.getData().getPlayers()) {
+            if(player.getUsername().equals(guiManager.getOwner())) {
+                for (AssistantCard assistantCard : guiManager.getData().getCurrPlayer().getMyDeck().getCards()) {
+                    ImageView imageView = new ImageView(GuiManager.class.getResource("/graphics/playerItems/deck/assistantCards/Assistente ("+assistantCard.getValue()+").png").toString());
+                    imageView.setPreserveRatio(true);
+                    imageView.setFitHeight(0);
+                    imageView.setFitWidth(150);
+                    imageView.setOnMouseEntered(this::mouseOnCharacters);
+                    imageView.setOnMouseExited(this::mouseOffCharacters);
+                    AssistantCards.getChildren().add(imageView);
+                    AssistantCards.toFront();
+                }
+                
+            }
+
+        }
+    }
+
+    private void fillMyDiningRoom(){
+        for (Player player : guiManager.getData().getPlayers()) {
+            if (player.getUsername().equals(guiManager.getOwner())) {
+                for(int i=0;i<5;i++){
+                    player.getMySchoolBoard().addProfessor(new Professor(COLOR.values()[i]));
+                }
+                for (int i=0;i<5;i++){
+                    for(int j=0;j<player.getMySchoolBoard().getDiningRoom()[i].size();j++){
+                        ImageView imageView= new ImageView(GuiManager.class.getResource("/graphics/pieces/student_"+COLOR.values()[i].toString().toLowerCase()+".png").toString());
+                        imageView.setPreserveRatio(true);
+                        imageView.setFitHeight(35);
+                        imageView.setFitWidth(33);
+                        MyDiningRoom.add(imageView,i,j);
+                    }
+                }
+                for(int i=0;i<player.getMySchoolBoard().getEntrance().size();i++){
+                    COLOR color=player.getMySchoolBoard().getEntrance().get(i).getColor();
+                    ImageView imageView= new ImageView(GuiManager.class.getResource("/graphics/pieces/student_"+color.toString().toLowerCase()+".png").toString());
+                    imageView.setPreserveRatio(true);
+                    imageView.setFitHeight(35);
+                    imageView.setFitWidth(33);
+                    if(i%2==0)
+                        MyEntrance.add(imageView,1,i/2);
+                    else
+                        MyEntrance.add(imageView,0,i/2+1);
+                }
+                for(Professor professor: player.getMySchoolBoard().getProfessors()){
+                    COLOR color=professor.getColor();
+                    ImageView imageView= new ImageView(GuiManager.class.getResource("/graphics/pieces/teacher_"+color.toString().toLowerCase()+".png").toString());
+                    imageView.setPreserveRatio(true);
+                    imageView.setFitWidth(33);
+                    imageView.setRotate(29.7);
+                    MyProfessors.add(imageView,0,color.ordinal());
+                }
+                for(int i=0;i<player.getMySchoolBoard().getTowers().size();i++){
+                    TEAM team=player.getTeam();
+                    ImageView imageView= new ImageView(GuiManager.class.getResource("/graphics/pieces/"+team.toString().toLowerCase()+"_tower.png").toString());
+                    imageView.setPreserveRatio(true);
+                    imageView.setFitWidth(80);
+                    if(i%2==0)
+                        MyTowers.add(imageView,0,i/2);
+                    else
+                        MyTowers.add(imageView,1,i/2);
+                }
+            }
+        }
+    }
+
+    private void fillOtherPlayers(){
+        int flag=0;
+        GridPane entrance=Player1Entrance,diningroom=Player1DiningRoom,professors=Player1Professors,towers=Player1Towers;
+        ImageView assistantCard=Player1AssistantCard;
+        for(Player player:guiManager.getData().getPlayers()){
+            if(!player.getUsername().equals(guiManager.getOwner())){
+                if(flag!=0){
+                    entrance=Player2Entrance;
+                    diningroom=Player2DiningRoom;
+                    professors=Player2Professors;
+                    towers=Player2Towers;
+                    assistantCard=Player2AssistantCard;
+                }
+                for(int i=0;i<5;i++){
+                    player.getMySchoolBoard().addProfessor(new Professor(COLOR.values()[i]));
+                }
+                for (int i=0;i<5;i++){
+                    for(int j=0;j<player.getMySchoolBoard().getDiningRoom()[i].size();j++){
+                        ImageView imageView= new ImageView(GuiManager.class.getResource("/graphics/pieces/student_"+COLOR.values()[i].toString().toLowerCase()+".png").toString());
+                        imageView.setPreserveRatio(true);
+           //             imageView.setFitHeight(35);
+                        imageView.setFitWidth(33);
+                        diningroom.add(imageView,i,j);
+                    }
+                }
+                for(int i=0;i<player.getMySchoolBoard().getEntrance().size();i++){
+                    COLOR color=player.getMySchoolBoard().getEntrance().get(i).getColor();
+                    ImageView imageView= new ImageView(GuiManager.class.getResource("/graphics/pieces/student_"+color.toString().toLowerCase()+".png").toString());
+                    imageView.setPreserveRatio(true);
+                   // imageView.setFitHeight(35);
+                    imageView.setFitWidth(33);
+                    if(i%2==0)
+                        entrance.add(imageView,1,i/2);
+                    else
+                        entrance.add(imageView,0,i/2+1);
+                }
+                for(Professor professor: player.getMySchoolBoard().getProfessors()){
+                    COLOR color=professor.getColor();
+                    ImageView imageView= new ImageView(GuiManager.class.getResource("/graphics/pieces/teacher_"+color.toString().toLowerCase()+".png").toString());
+                    imageView.setPreserveRatio(true);
+                    imageView.setFitWidth(33);
+                    imageView.setRotate(29.7);
+                    professors.add(imageView,0,color.ordinal());
+                }
+                for(int i=0;i<player.getMySchoolBoard().getTowers().size();i++){
+                    TEAM team=player.getTeam();
+                    ImageView imageView= new ImageView(GuiManager.class.getResource("/graphics/pieces/"+team.toString().toLowerCase()+"_tower.png").toString());
+                    imageView.setPreserveRatio(true);
+                    imageView.setFitWidth(80);
+                    if(i%2==0)
+                        towers.add(imageView,0,i/2);
+                    else
+                        towers.add(imageView,1,i/2);
+                }
+                if(flag==0)
+                    player.setChosenCard(new AssistantCard(8,3));
+                else
+                    player.setChosenCard(new AssistantCard(6,2));
+                if(player.getChosenCard()!=null){
+                    Image image= new Image(GuiManager.class.getResource("/graphics/playerItems/deck/assistantCards/Assistente ("+player.getChosenCard().getValue()+").png").toString());
+                    assistantCard.setImage(image);
+                }
+            flag++;
+            }
+        }
     }
 }
