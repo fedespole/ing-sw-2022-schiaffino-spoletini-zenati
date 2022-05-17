028f113e3d5a5c2b385caf16a046000c8c5dfeac
AlbertoSchiaffino <alberto1.schiaffino1@mail.polimi.it>
2022-05-17 18:48:36 +0200

    Planning scene

--- src/main/java/it/polimi/ingsw/view/gui/controllers/PlanningController.java
+++ src/main/java/it/polimi/ingsw/view/gui/controllers/PlanningController.java
@@ -1,27 +1,23 @@
 package it.polimi.ingsw.view.gui.controllers;
 
 import it.polimi.ingsw.common.events.fromClientEvents.DrawAssistantCardEvent;
-import it.polimi.ingsw.common.events.fromClientEvents.PlayerAccessEvent;
 import it.polimi.ingsw.common.events.fromServerEvents.UpdatedDataEvent;
 import it.polimi.ingsw.model.basicgame.*;
 import it.polimi.ingsw.model.basicgame.playeritems.AssistantCard;
 import it.polimi.ingsw.model.basicgame.playeritems.Player;
+import it.polimi.ingsw.model.expertgame.characters.Character;
 import it.polimi.ingsw.view.gui.Constants;
 import it.polimi.ingsw.view.gui.GuiManager;
 import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.geometry.Pos;
 import javafx.scene.Cursor;
-import javafx.scene.Node;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.GridPane;
-
-import java.io.File;
-import java.util.ArrayList;
 
 public class PlanningController extends GuiController{
     public FlowPane AssistantCards;
@@ -42,6 +38,8 @@ public class PlanningController extends GuiController{
     public ImageView Player2AssistantCard;
     public Label Title;
     public ImageView ChosenCard;
+    public ImageView CharacterBack;
+    public FlowPane Characters;
 
     @FXML
     @Override
@@ -56,22 +54,40 @@ public class PlanningController extends GuiController{
         this.fillMyDiningRoom();
         this.addAvailableAssistantCards();
         this.fillOtherPlayers();
+        if(guiManager.getData().isExpert())
+            this.setCharacters();
     }
 
-    public void mouseOnCharacters(MouseEvent mouseEvent) {
+    public void mouseOnAssistants(MouseEvent mouseEvent) {
         if(guiManager.getOwner().equals(guiManager.getData().getCurrPlayer().getUsername()))
             AssistantCards.getScene().setCursor(Cursor.HAND);
         AssistantCards.alignmentProperty().setValue(Pos.TOP_CENTER);
     }
 
 
-    public void mouseOffCharacters(MouseEvent mouseEvent) {
+    public void mouseOffAssistants(MouseEvent mouseEvent) {
         AssistantCards.alignmentProperty().setValue(Pos.BOTTOM_CENTER);
         if(AssistantCards.getScene()!=null)
             AssistantCards.getScene().setCursor(Cursor.DEFAULT);
     }
+    public void mouseOffCharacters(MouseEvent mouseEvent){
+        Characters.setVisible(false);
+        CharacterBack.setVisible(true);
+        AssistantCards.getScene().setCursor(Cursor.DEFAULT);
+    }
 
+    public void mouseOnCharacterBack(MouseEvent mouseEvent){
+        AssistantCards.getScene().setCursor(Cursor.HAND);
+    }
+    public void mouseClickedBackCharacter(MouseEvent mouseEvent){
+        Characters.setVisible(true);
+        CharacterBack.setVisible(false);
+        Characters.getScene().setCursor(Cursor.HAND);
+    }
     public void mouseClickedCharacter(MouseEvent mouseEvent){
+        int valueChar=Integer.parseInt(((ImageView) mouseEvent.getSource()).getId());
+    }
+    public void mouseClickedAssistant(MouseEvent mouseEvent){
         if(guiManager.getOwner().equals(guiManager.getData().getCurrPlayer().getUsername())) {
             ((ImageView) mouseEvent.getSource()).setOpacity(0);
             int valueCard = Integer.parseInt(((ImageView) mouseEvent.getSource()).getId());
@@ -85,12 +101,11 @@ public class PlanningController extends GuiController{
                 for (AssistantCard assistantCard : player.getMyDeck().getCards()) {
                     ImageView imageView = new ImageView(GuiManager.class.getResource("/graphics/playerItems/deck/assistantCards/Assistente ("+assistantCard.getValue()+").png").toString());
                     imageView.setPreserveRatio(true);
-                    imageView.setFitHeight(0);
                     imageView.setFitWidth(100);
-                    imageView.setOnMouseEntered(this::mouseOnCharacters);
-                    imageView.setOnMouseExited(this::mouseOffCharacters);
+                    imageView.setOnMouseEntered(this::mouseOnAssistants);
+                    imageView.setOnMouseExited(this::mouseOffAssistants);
                     if(player.getUsername().equals(guiManager.getData().getCurrPlayer().getUsername()))
-                        imageView.setOnMouseClicked(this::mouseClickedCharacter);
+                        imageView.setOnMouseClicked(this::mouseClickedAssistant);
                     imageView.setId(Integer.toString(assistantCard.getValue()));
                     AssistantCards.getChildren().add(imageView);
                     AssistantCards.toFront();
@@ -209,6 +224,22 @@ public class PlanningController extends GuiController{
         }
     }
 
+    private void setCharacters(){
+        this.CharacterBack.setImage(new Image(GuiManager.class.getResource("/graphics/characters/Personaggi_retro.jpg").toString()));
+        this.CharacterBack.setOnMouseClicked(this::mouseClickedBackCharacter);
+        this.CharacterBack.setOnMouseEntered(this::mouseOnCharacterBack);
+        this.CharacterBack.toFront();
+        this.Characters.setVisible(false);
+        this.Characters.setOnMouseExited(this::mouseOffCharacters);
+        for(Character character: guiManager.getData().getCharacters()){
+            ImageView imageView = new ImageView(GuiManager.class.getResource("/graphics/characters/CarteTOT_front"+character.getId()+".jpg").toString());
+            imageView.setPreserveRatio(true);
+            imageView.setFitWidth(100);
+            imageView.setOnMouseClicked(this::mouseClickedCharacter);
+            imageView.setId(Integer.toString(character.getId()));
+            Characters.getChildren().add(imageView);
+        }
+    }
     @Override
     public void update(UpdatedDataEvent event) {
         if(guiManager.getData().getStatusGame().getStatus().equals(STATUS.PLANNING))
