8e5f627691b26469e851a52ff46ffbba0cba3aa4
Alessandro Zenati <alessandro.zenati@mail.polimi.it>
2022-05-18 17:08:55 +0200

    Mod

--- src/main/java/it/polimi/ingsw/view/gui/controllers/CharacterInfoController.java
+++ src/main/java/it/polimi/ingsw/view/gui/controllers/CharacterInfoController.java
@@ -24,6 +24,7 @@ public class CharacterInfoController extends GuiController{
     @FXML
     @Override
     public void initialize(){
+        effectText = new Label();
         effectText.setText("");
     }
 
